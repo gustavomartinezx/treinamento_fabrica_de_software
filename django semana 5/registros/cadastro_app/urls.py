12d772a1504a8167709a50803c7e048af231e5ae
Gustavo Palmeira de Arruda Martinez <128634952+gustavomartinezx@users.noreply.github.com>
2023-06-06 15:24:37 -0300

    Delete django semana 5 directory

--- django semana 5/registros/cadastro_app/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from cadastro_app.views import CadastroViewSet
-
-router = routers.DefaultRouter()
-router.register(r'', CadastroViewSet)
-
-urlpatterns = router.urls
